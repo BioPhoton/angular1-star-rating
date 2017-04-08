212932c47dc2bf8deff6cd24f2eec9c39fef4c82
Michael Hladky <michael@hladky.at>
2017-04-08 20:48:35 +0200

    feat(helper function): implement basic helper function for ionic release

--- src/star-rating.controller.ts
+++ src/star-rating.controller.ts
@@ -493,7 +493,6 @@ export class StarRatingController implements ng.IComponentController, IStarRatin
         }
 
         this.hoverRating = rating?parseInt(rating.toString()):0;
-        console.log('onStarHover: ', this.hoverRating);
         //fire onHover event
         let $event:IStarRatingOnHoverEvent = { hoverRating: this.hoverRating};
         if(typeof this.onHover === 'function') {
@@ -504,6 +503,40 @@ export class StarRatingController implements ng.IComponentController, IStarRatin
 
     protected interactionPossible():boolean {
         return  !this.readOnly && !this.disabled;
+    }
+
+    /**
+     * Get the closest matching element up the DOM tree.
+     * @private
+     * @param  {Element} elem     Starting element
+     * @param  {String}  selector Selector to match against
+     * @return {Boolean|Element}  Returns null if not match found
+     */
+    protected getClosest ( elem, selector ) {
+
+        // Element.matches() polyfill
+        if (!Element.prototype.matches) {
+            Element.prototype.matches =
+                Element.prototype.matchesSelector ||
+                Element.prototype.mozMatchesSelector ||
+                Element.prototype.msMatchesSelector ||
+                Element.prototype.oMatchesSelector ||
+                Element.prototype.webkitMatchesSelector ||
+                function (s) {
+                    var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                        i = matches.length;
+                    while (--i >= 0 && matches.item(i) !== this) {
+                    }
+                    return i > -1;
+                };
+        }
+
+        // Get closest match
+        for (; elem && elem !== document; elem = elem.parentNode) {
+            if (elem.matches(selector)) return elem;
+        }
+
+        return null;
     }
 
 }
